b80bb7659fa3460ff64a0833ef5fd5784209f76b
Sean Gilbert <getonthetrail@gmail.com>
2013-02-11 15:19:49 -0500

    new packages for continuous test tools

---
+++ ncrunch.vs2010/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'ncrunch.vs2010' 
+$installerType = 'MSI' 
+$url = 'http://downloads.ncrunch.net/NCrunch_VS2010_1.44.0.11.msi' download url
+$url64 = $url 
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
---
+++ ncrunch.vs2012/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'ncrunch.vs2012' 
+$installerType = 'MSI' 
+$url = 'http://downloads.ncrunch.net/NCrunch_VS2012_1.44.0.11.msi'  download url
+$url64 = $url 
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
