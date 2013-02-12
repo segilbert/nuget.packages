f0c24287caa5379640610c9e278cdbfd48a161e8
Sean Gilbert <getonthetrail@gmail.com>
2013-02-11 22:09:41 -0500

    datastax community cassandra

---
+++ datatax.community/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿
+$packageName = 'datatax.community' 
+$installerType = 'MSI' 
+$url = 'http://downloads.datastax.com/community/datastax-community-32bit.msi' 
+$url64 = 'http://downloads.datastax.com/community/datastax-community-64bit.msi' 
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
