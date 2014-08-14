599116ee7927678ab1de72c036a2b19e35f89161
Sören Kuklau <soeren@kuklau.name>
2014-08-14 23:55:16 +0200

    VisualSVN Server 2.7.8

--- visualsvnserver/tools/chocolateyInstall.ps1
+++ visualsvnserver/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿
-$packageName = 'visualsvnserver' 
-$installerType = 'MSI' 
-$url = 'http://www.visualsvn.com/files/VisualSVN-Server-2.7.1.msi' 
-$url64 = $url 
-$silentArgs = '' 
-$validExitCodes = @(0) 
+$packageName = 'visualsvnserver'
+$installerType = 'MSI'
+$url = 'http://www.visualsvn.com/files/VisualSVN-Server-2.7.8.msi'
+$url64 = $url
+$silentArgs = ''
+$validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
--- visualsvnserver/tools/chocolateyUninstall.ps1
+++ visualsvnserver/tools/chocolateyUninstall.ps1
@@ -1,5 +1,7 @@
-﻿$package = 'ncrunch.vs2012'
-#$file = (Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{421F9DC0-5A07-4F05-91BD-4801714EE08C}").UninstallString;
+﻿$packageName = 'visualsvnserver'
+$installerType = 'MSI'
+$silentArgs = ''
+
 $file = '{421F9DC0-5A07-4F05-91BD-4801714EE08C}'
 Uninstall-ChocolateyPackage $package 'MSI' -file $file
 
