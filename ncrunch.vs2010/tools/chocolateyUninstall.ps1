87ad9fc39f2eec875939b8cc5f11ed9375f2b977
Daniel Hilgarth <d.hilgarth@fire-development.com>
2015-09-27 18:41:27 +0200

    Deprecate NCrunch packages

--- ncrunch.vs2010/tools/chocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$package = 'ncrunch.vs2010'
-$file = (Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{421F9DC0-5A07-4F05-91BD-4801714EE08C}").UninstallString;
-Uninstall-ChocolateyPackage $package 'MSI' -file $file
-
-
